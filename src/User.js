d6bdb9f34024068534f30380fb1b015d70ae7ad0
Ataman80 <if190480aar@gmail.comm>
2022-03-18 22:24:24 +0200

    user

---
+++ src/User.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class User extends Component {
+    state={user:null}
+    componentDidMount() {
+
+        let{id}=this.props
+        console.log(id)
+        fetch('https://jsonplaceholder.typicode.com/users'+"/"+id)
+                .then(response => response.json())
+                .then(json => this.setState({user:json}))
+
+
+
+    }
+
+    render() {
+        let{user}=this.state
+        return (
+            <div>
+                {user&&<div>{user.id}--{user.name}</div>}
+            </div>
+        );
+    }
+}
+
+export default User;
