f840ef65586a0500b64cd331e27711c257f0ed74
Ataman80 <if190480aar@gmail.comm>
2022-03-18 22:23:39 +0200

    input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import User from "./User";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {inputValue: 1, user: false}
+    inputMain = React.createRef()
+    onFormSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.inputMain.current.value)
+
+
+    }
+    onInputFill = () => {
+
+        this.setState({inputValue: this.inputMain.current.value})
+        this.setState({user: false})
+
+    }
+    onClick = () => {
+        this.setState({user: true})
+
+    }
+
+    render() {
+        let {inputValue, user} = this.state;
+
+        return (
+            <div>
+                <form onSubmit={this.onFormSubmit}>
+                    <input ref={this.inputMain} onInput={this.onInputFill} type={'number'} value={inputValue}/>
+                    {((inputValue < 11) && (inputValue > 0)) && (<button onClick={this.onClick} >send</button>)                   }
+
+ {user && <User id={inputValue} key={inputValue}/>  }
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
