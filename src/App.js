9cb064f4a9ebc6eeaef9642abd48c0187580c2b8
Ataman80 <if190480aar@gmail.comm>
2022-03-23 21:49:50 +0200

    input

--- src/App.js
+++ src/App.js
@@ -1,52 +1,39 @@
-import React, {useEffect, useReducer, useState} from 'react';
-const initialstate=null
-function reducer(state, action){
-    switch (action.type){
-        case "todo":{
-            return action.payload
-        }
-        case "name":{
-           return{...state,
-                name:action.payload}
-        }
-        default: {
-            console.log("error")
-        }
-    }
+import React, {Component, useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {fetchTodos} from "./redux/action-types/todos-action";
 
-}
-export default function App() {
-    const [count, SetCount] = useState(1)
-    const [inputValue, setValue]=useState("")
-    const[state, dispatch]=useReducer(reducer, initialstate)
-    const inc = () => {
-        SetCount((val) => val + 1)
+export default function App(){
+    const {counter, todos}=useSelector(({counter:{counter}, todos:{todos}})=>({counter, todos}))
+    const dispatch=useDispatch();
+    const inc=()=>{
+        dispatch({type:"INC"})
+    }
+    const dec=()=>{
+        dispatch({type:"DEC"})
     }
     const res=()=>{
-        SetCount(0)
+        dispatch({type:"RES"})
     }
-    const fetchData=()=>{
-        fetch('https://jsonplaceholder.typicode.com/users/'+ count)
-            .then(response => response.json())
-            .then(json => dispatch({type:"todo", payload: json }))
+    // const fetchData=()=>{
+    //     try{    fetch
+    // ('https://jsonplaceholder.typicode.com/todos')
+    //     .then(response => response.json())
+    //     .then(json => dispatch({type:"Todos", payload:json}))
+    //     }
+    //     catch (e) {
+    //        console.log(e)
+    //     }
+    // }
 
 
-    }
-    const nameChange=()=>{
-        dispatch({type:"name", payload:inputValue })
-    }
-    useEffect(()=>{
-        fetchData();
-    }, [count])
-    return (
-        <div>
-            <h1> count {count}</h1>
-            <button onClick={inc}>inc</button>
-            <button onClick={res}>res</button>
-            <input value={inputValue} onChange={({target:{value}})=>setValue(value)}/>
-            <button onClick={nameChange}>name</button>
-            {!!state&&<div>{state.id}--{state.name}-{state.username}</div>}
-
-        </div>
-    );
+    useEffect(()=>{fetchTodos(dispatch)}, [])
+return(
+<div>
+    <h1>counter{counter}</h1>
+    <button onClick={inc}>inc</button>
+    <button onClick={dec}>dec</button>
+    <button onClick={res}>res</button>
+    {todos.map(todos=><h1>{todos.id}--{todos.title}</h1>)}
+</div>
+);
 }
